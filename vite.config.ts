e65e6f0f3e0cd139056514cc14a12c9d607a37e4
Levi-lang-max <kebron23belay@gmail.com>
2026-05-10 04:34:26 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,3 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 });
+{
+  "compilerOptions": {
+    "baseUrl": ".",
+    "paths": {
+      "@/*": ["src/*"]
+    }
+  }
+}
