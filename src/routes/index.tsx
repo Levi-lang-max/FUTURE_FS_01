aa0a01a51af693e81d0a6f87c9c3ae43f16a6617
Levi-lang-max <kebron23belay@gmail.com>
2026-05-10 05:09:23 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Toaster } from "sonner";
-import { Navbar } from "@/components/Navbar";
+import { Navbar } from "../components/Navbar";
 import { Hero } from "@/components/Hero";
 import { About } from "@/components/About";
 import { Projects } from "@/components/Projects";
