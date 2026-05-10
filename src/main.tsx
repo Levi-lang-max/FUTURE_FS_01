ff3cbb465e31ea84a44eb8d4048804a5dfa51908
Levi-lang-max <kebron23belay@gmail.com>
2026-05-10 04:26:02 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>FUTURE FS 01 🚀</h1>
-      <p>SPA Deployment Fixed Successfully</p>
-    </div>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
